b04f26f5d0a07685c3222271c626215dcd20bc3d
Kevin <trupiakevin@gmail.com>
2025-12-11 23:01:34 +0100

    feat: aggiunto all'esercizio il bottone spegni con il relativo codice js per lo spegnimento

--- script.js
+++ script.js
@@ -7,19 +7,33 @@ Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare 
 
 
 //  cerco nella pagina html e prendo  l'elemento con l'id= "accensione" lo trova e lo assegna alla costante button 
-const button = document.getElementById("accensione");
+const buttonAccendi = document.getElementById("accensione");
+const buttonSpegni = document.getElementById("spegnimento")
+
 
 // cerco e  Prendo l'elemento  in questo caso l'img con id=lampadina  e faccio la stessa cosa della riga precedente 
 
 const lampadina = document.getElementById("lampadina");
+const lampadinaAccesa = document.getElementById("lampadina accesa")
 
 // registro cosa succede quando l'utente clicca il bottone. quando l'elemento button riceve un click, esegui la funzione 
 // addEventListener registra per il click del bottone
  
-button.addEventListener("click", () => {
+buttonAccendi.addEventListener("click", () => {
 
     // dentro la funzione di click imposto lampadina.src = img/yellow_lamp.png cosi il browser carica questa immagine e la mostra 
     // al posto di quella precedente.
      
     lampadina.src = "img/yellow_lamp.png";
 });
+
+
+
+buttonSpegni.addEventListener("click", () => {
+     
+    lampadina.src = "img/white_lamp.png";
+});
+
+
+
+
