d712b03eadea1b5f6dcf226b43e673ef84c2e9f2
Kevin <trupiakevin@gmail.com>
2025-12-11 23:07:16 +0100

    fix: esercizio

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const buttonSpegni = document.getElementById("spegnimento")
 // cerco e  Prendo l'elemento  in questo caso l'img con id=lampadina  e faccio la stessa cosa della riga precedente 
 
 const lampadina = document.getElementById("lampadina");
-const lampadinaAccesa = document.getElementById("lampadina accesa")
+
 
 // registro cosa succede quando l'utente clicca il bottone. quando l'elemento button riceve un click, esegui la funzione 
 // addEventListener registra per il click del bottone
